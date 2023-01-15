83d3208283675670ca7faef850059d08815f738c
gabriellondero40@gmail.com <gabriellondero40@gmail.com>
2023-01-15 16:46:35 -0300

    incluideUseRef

--- src/components/container/Father.jsx
+++ src/components/container/Father.jsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Child from '../pure/Child'
 
 const Father = () => {
+  
+  const [state, setNewState] = useState('Gabriel')
 
+  // generamos la funcion para que traiga los datos del props del
+  // componente "Child", luego los renderice a traves del metodo "alert()" alert 
+  // para que se imprima como una ventana emergente en pantalla.
+  
   const sendMessage = (text) => {
     alert(`Message: ${text}`)
   }
 
+  const receiveMessage = (newName) => {
+    setNewState(newName)
+  }
+
   return (
     <div>
-      <Child send={sendMessage}/>
+      <Child name={state} send={sendMessage} upName={receiveMessage} />
     </div>
   )
 }
--- src/components/pure/Child.jsx
+++ src/components/pure/Child.jsx
@@ -1,9 +1,11 @@
 import React, { useRef } from 'react'
 
 
-const Child = ({ name, send }) => {
+
+const Child = ({ name, send, upName }) => {
 
   const messageRef = useRef('');
+  const nameRef = useRef('')
     
   const pressButtonTwo = () => {
     const content = messageRef.current.value;
@@ -12,26 +14,47 @@ const Child = ({ name, send }) => {
     
     const pressButtonThree = (text) => {
         alert(`Hello: ${text}`);
-    }
+  }
+  
+  const sendData = (e) => {
+    e.preventDefault()
+    upName(nameRef.current.value)
+  }
     
   return (
     <div>
-      <p onMouseOver={() => console.log('On Mouse Over')}> Child Component</p>
+      <p onMouseOver={() => console.log('On Mouse Over')}> Hello {name}</p>
           <button onClick={() => console.log('Press Button 1')}>
               Press Button 1
           </button>
           <button onClick={pressButtonTwo}>Press Button Two
-          </button>
+      </button>
+      {/* cuando hay paramentros que pasar, el evento comienza con un 
+      arrowFunction */}
           <button onClick={() => pressButtonThree('gabriel')}>Press Button Three
       </button>
       <input placeholder='insert text'
         onFocus={() => console.log('Input Focused')}
         onChange={(e) => console.log('Input Change:', e.target.value)}
         onCopy={() => console.log('Copied text from Input')} 
+        // onFocus-> cada vez que se enfoca la casilla genera el evento
+        // onChange -> cada cambio maniefiesta el evento
+        // onCopy -> copia los valores que se toma
+        
         ref={messageRef}
-        /> 
+      /> 
 
-      <button onClick={() => send('Hello Papi')}/>
+      {/* el send esta llamando a traves del evento onclick a la variable con el valor que trae 
+      * el Hooks "useRef()" accediendo a traves del "current.value"
+      */}
+      <button onClick={() => send(messageRef.current.value)}>Send the message</button>
+
+      <form onSubmit={sendData}>
+        <input ref={nameRef} placeholder='Write your Name' 
+
+        />
+        <button type='submit'>Submit data</button>
+      </form>
     </div>
   )
 }
